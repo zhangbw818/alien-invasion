d77a3e907a477a00c8e830dc6c4edce2564ddc8b
zhangbw818 <66865413+zhangbw818@users.noreply.github.com>
2020-07-11 11:42:10 +0800

    Add files via upload
    创建类存储游戏所有设置的类

---
+++ alien_invasion.py
@@ -0,0 +1,24 @@
+import pygame
+import sys
+from settings import Settings
+
+def run_game():
+    """初始化pygame、设置和屏幕对象"""
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #开始游戏的主循环
+    while True:
+        #监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        #每次循循环时都重绘屏幕
+        screen.fill(ai_settings.bg_color)
+
+        #让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
